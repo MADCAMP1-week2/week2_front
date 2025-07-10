b0b8ec3f05118155b8f3f49ecf66c1b5aa9892be
BaeYoona <jinalks43@gmail.com>
2025-07-11 00:32:05 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'light';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>이건 내가 쓴 거여~!!</Text>
+          <Text>이것도 내가 쓴 거야~!!</Text>
+          <Text>그냥 저장만 함</Text>
+          <Text>폰에서도 해 봄</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
