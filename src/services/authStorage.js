b46509c42bb9f023d29fa0a582c650a5671c89b7
BaeYoona <jinalks43@gmail.com>
2025-07-13 19:56:07 +0900

    [Fix] authStorage.js 토큰 핸들링 개선

--- src/services/authStorage.js
+++ src/services/authStorage.js
@@ -1,35 +1,38 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const TOKEN_KEY = 'tokens';
+
 const authStorage = {
-  storeToken: async (accessToken, refreshToken, user) => {
+  storeToken: async ({accessToken, refreshToken, user}) => {
+    const tokenObj = {accessToken, refreshToken, user};
     try {
-      await AsyncStorage.setItem('accessToken', accessToken);
-      await AsyncStorage.setItem('refreshToken', refreshToken);
-      await AsyncStorage.setItem('id', user.id);
+      await AsyncStorage.setItem(TOKEN_KEY, JSON.stringify(tokenObj));
     } catch (error) {
       console.error('❌ 토큰 저장 중 오류가 발생했습니다.', error);
+      throw error;
     }
   },
 
+  // 저장된 토큰 반환
   getToken: async () => {
     try {
-      const accessToken = await AsyncStorage.getItem('accessToken');
-      const refreshToken = await AsyncStorage.getItem('refreshToken');
-      const id = await AsyncStorage.getItem('id');
-      return {accessToken: accessToken, refreshToken: refreshToken, id: id};
+      const json = await AsyncStorage.getItem(TOKEN_KEY);
+      return json
+        ? JSON.parse(json)
+        : {accessToken: null, refreshToken: null, user: null};
     } catch (error) {
       console.error('❌ 토큰을 불러오는 중 오류가 발생했습니다.', error);
-      return {accessToken: null, refreshToken: null, id: null};
+      return {accessToken: null, refreshToken: null, user: null};
     }
   },
 
-  removeToken: async () => {
+  // 토큰 삭제
+  clearToken: async () => {
     try {
-      await AsyncStorage.removeItem('accessToken');
-      await AsyncStorage.removeItem('refreshToken');
-      await AsyncStorage.removeItem('id');
+      await AsyncStorage.removeItem(TOKEN_KEY);
     } catch (error) {
       console.error('⚠️ 토큰 삭제 중 오류가 발생했습니다.', error);
+      throw error;
     }
   },
 };
