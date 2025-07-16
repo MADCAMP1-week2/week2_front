c334055cbf664fe1b1bb784b35b2cc24dd231816
BaeYoona <jinalks43@gmail.com>
2025-07-16 16:41:07 +0900

    fix: 일정 불러오기 오류 고침

--- src/hooks/useSchedules.js
+++ src/hooks/useSchedules.js
@@ -1,10 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import {useQuery} from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { fetchSchedules } from '@api/schedules';
+import {fetchSchedules} from '@api/schedules';
 
 export const useSchedules = (date, projectId) => {
-  const start = dayjs(date).startOf('day').toISOString(); // 00:00:00
-  const end = dayjs(date).endOf('day').toISOString(); // 23:59:59.999
+  const start = dayjs(date).startOf('day').format('YYYY-MM-DD');
+  const end = dayjs(date).endOf('day').format('YYYY-MM-DD');
   return useQuery({
     queryKey: ['schedules', start, end, projectId],
     queryFn: () => fetchSchedules(start, end, projectId),
@@ -14,12 +14,15 @@ export const useSchedules = (date, projectId) => {
 };
 
 export const useWeekSchedules = (weekDate, projectId) => {
-  const start = dayjs(weekDate).startOf('week').startOf('day').toISOString();
-  const end = dayjs(weekDate).endOf('week').endOf('day').toISOString(); // 23:59:59.999
+  const start = dayjs(weekDate)
+    .startOf('week')
+    .startOf('day')
+    .format('YYYY-MM-DD');
+  const end = dayjs(weekDate).endOf('week').endOf('day').format('YYYY-MM-DD');
   return useQuery({
     queryKey: ['schedules', start, end, projectId],
     queryFn: () => fetchSchedules(start, end, projectId),
-    enabled: !!date, // date 없을 땐 fetch 안 함
+    enabled: !!weekDate, // date 없을 땐 fetch 안 함
     staleTime: 1000 * 60 * 5, // 5분 동안 fresh
   });
 };
@@ -33,8 +36,7 @@ export const useMonthSchedules = (monthDate, projectId) => {
   return useQuery({
     queryKey: ['schedules', start, end, projectId],
     queryFn: () => fetchSchedules(start, end, projectId),
-    enabled: !!date, // date 없을 땐 fetch 안 함
+    enabled: !!monthDate, // date 없을 땐 fetch 안 함
     staleTime: 1000 * 60 * 5, // 5분 동안 fresh
   });
 };
-
