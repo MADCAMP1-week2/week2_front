9b6fa93793f1247edd389fd28d610ef8ff000912
김민준 <kimminjun0215@gmail.com>
2025-07-16 18:05:06 +0900

    fix: todo list design

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet, Text} from 'react-native';
 import {useTodos} from '@hooks/useTodos';
 import TodoItem from './TodoItem';
+import { useHomeUIStore } from '@store/homeUIStore';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+
+dayjs.locale('ko'); // 한글 요일
 
 const TodoList = ({date, projectId}) => {
   const {data: todos = []} = useTodos(date, projectId);
   const renderItem = ({item}) => <TodoItem todo={item} onToggle={() => {}} />;
+  const selectedDate = useHomeUIStore(state => state.selectedDate);
+  const formattedDate = dayjs(selectedDate).format('M.D.(dd)');
   return (
     <FlatList
       data={todos}
       keyExtractor={item => item._id.toString()}
       renderItem={renderItem}
-      contentContainerStyle={{paddingHorizontal: 16}}
+      ListHeaderComponent={
+        <Text style={{ 
+          fontSize: 13, 
+          color: "#6f6f6fff", 
+          marginBottom: 5,
+          left: 10,
+          fontFamily: 'SCDream-Bold' 
+        }}>
+          {formattedDate}
+        </Text>
+      }
+      contentContainerStyle={{ paddingHorizontal: 10, paddingVertical: 15 }}
       style={styles.container}
     />
   );
--- src/components/TopBar/Score.js
+++ src/components/TopBar/Score.js
@@ -7,20 +7,20 @@ import Svg, { Defs, RadialGradient, Rect, Stop } from 'react-native-svg';
 const Score = ({ userName = '윤아', score = 589, onRefresh }) => {
   return (
     <View style={{ paddingHorizontal: 16, marginTop: 12, justifyContent: 'center', alignItems: 'center' }}>
-      {/* 인사말 */}
+
       <Text style={{ fontSize: 20, fontFamily: "SCDream-Medium", color: "#262626" }}>
         {userName}님, 안녕하세요!
       </Text>
 
       {/* 점수 + 새로고침 */}
-      <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-        <Text style={{ fontSize: 48,  color: '#C90000', fontFamily: "SCDream-ExtraBold" }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 0 }}>
+        <Text style={{ fontSize: 50,  color: '#C90000', fontFamily: "SCDream-ExtraBold" }}>
           {score}
         </Text>
       </View>
 
       {/* 경고 텍스트 */}
-      <Text style={{ color: 'red', color: '#C90000', fontFamily: "SCDream-Bold" }}>
+      <Text style={{ color: 'red', color: '#C90000', fontFamily: "SCDream-Bold", fontSize: 15, marginBottom: 13 }}>
         서두르세요! 할 일이 많습니다!
       </Text>
 
